e5280a30ba5ccce6fa77f0c3397be2c2deeb5a66
navneet23341 <163036297+navneet23341@users.noreply.github.com>
2024-09-01 03:25:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-window.onload = () => {
-    const startButton = document.getElementById('startButton');
-    const content = document.getElementById('content');
-
-    startButton.addEventListener('click', () => {
+onload = () =>{
         document.body.classList.remove("container");
-        content.style.display = 'block'; // Show the content
-        startButton.style.display = 'none'; // Hide the button
-    });
 };
